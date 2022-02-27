fa54afc509c88a36a470ff63e76009c3d7ec0ae2
BM00786 <biswa.bm60@gmail.com>
2022-02-27 14:20:17 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+clevertap.onUserLogin.push({
+    "Site": {
+      "Name": "Biswaweb1",
+      "Identity": 12121,
+      "Email": "sBiswaweb1@gmail.com",
+      "Gender": "M",
+      "DOB": new Date(),
+      "MSG-email": true,
+      "MSG-push": true,
+      "MSG-sms": true,
+      "MSG-whatsapp": true,
+    },
+  });
